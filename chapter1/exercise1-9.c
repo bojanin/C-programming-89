86e8d57d7c55cf5a49b7430236afcf1a02b08cbc
ven1xus <bojanint@gmail.com>
2019-01-07 21:30:36 -0500

    added chapter 1

---
+++ chapter1/exercise1-9.c
@@ -0,0 +1,29 @@
+/* -- -- -- -- -- -- -- -- -- -- -- -- --  
+
+* File Name : spaceReplacer.c
+
+* Purpose : Write a program to copy its input to its output, replacing each string
+* of one or more spaces by a single space. DONT USE SCANF/PRINTF
+
+* Creation Date : 07-01-2019
+
+* Last Modified : Mon Jan  7 21:28:10 2019
+
+* Created By : @ven1xus 
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+#include <stdio.h>
+
+
+int main(void) {
+    char c;
+    while((c = getchar()) != EOF) {
+        if (c == ' ' ) putchar(c);
+        while( c == ' ' && (c != EOF)) {
+            c = getchar();
+        }
+        putchar(c);
+    }
+    return 0;
+}
