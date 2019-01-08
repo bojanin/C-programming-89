49a936a2f1c9821ea5f07dcb5d04fbf0f1136e09
ven1xus <bojanint@gmail.com>
2019-01-07 23:15:03 -0500

    problem22

---
+++ chapter1/exercise1-22.c
@@ -0,0 +1,34 @@
+/**
+ * @author      : tommy (tommy@ven1xus-mbp)
+ * @file        : exercise1-22
+ * @created     : Monday Jan 07, 2019 22:58:01 EST
+ */
+
+#include <stdio.h>
+
+#define MAX_COLUMN 35
+#define MAX_LENGTH 1000
+
+int main(void) {
+    char input[MAX_LENGTH];
+    
+    char c;
+    int len;
+
+    while(len < MAX_LENGTH && (c = getchar()) != '\n') {
+        if(c != '\t'  && c != '\b' && c != ' ') {
+            input[len] = c;
+        } else {
+            input[len] = '/';
+        }
+        len++; 
+    } 
+    input[len] = '\0';
+    
+    for(int i = 0; input[i] != '\0';i++) {
+        if(i%MAX_COLUMN == 0) printf("\n");
+        putchar(input[i]);
+    }
+    printf("\n");
+    return 0;
+}
